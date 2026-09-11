83aacd711aa965052e10f7b9d6b228f200b8f82f
Krynode <ikhwanromadon943@gmail.com>
2026-09-12 06:01:47 +0700

    Upload 19 file(s) via RepoHub

---
+++ src/components/scroll/SmoothScroll.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import Lenis from 'lenis';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const SmoothScroll = ({ children }) => {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.1,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      smoothWheel: true,
+    });
+
+    window.__lenis = lenis;
+
+    lenis.on('scroll', ScrollTrigger.update);
+
+    const raf = (time) => {
+      lenis.raf(time * 1000);
+    };
+    gsap.ticker.add(raf);
+    gsap.ticker.lagSmoothing(0);
+
+    return () => {
+      gsap.ticker.remove(raf);
+      lenis.destroy();
+      window.__lenis = null;
+    };
+  }, []);
+
+  return <>{children}</>;
+};
+
+export default SmoothScroll;
