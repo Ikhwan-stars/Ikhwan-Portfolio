215a892dd17f58ea068333afad5380399d2b009b
Ikhwan Romadon <ikhwanromadon943@gmail.com>
2026-09-12 08:35:46 +0700

    Upload 29 file(s) via RepoHub

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Home from './pages/index';
 import SmoothScroll from './components/scroll/SmoothScroll';
+import Loading from './components/loading/loading';
+import ScrollTopButton from './components/scrollTop/ScrollTopButton';
 
 const App = () => {
+  const [loadingDone, setLoadingDone] = useState(false);
+
   return (
-    <SmoothScroll>
-      <Home />
-    </SmoothScroll>
+    <>
+      <Loading onFinish={() => setLoadingDone(true)} />
+      <SmoothScroll>
+        <Home />
+      </SmoothScroll>
+      {loadingDone && <ScrollTopButton />}
+    </>
   );
 };
 
