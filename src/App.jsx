9c8632eb0598c9a01e4fd0a44c9d1b5cb805691e
Ikhwan Romadon <ikhwanromadon943@gmail.com>
2026-09-17 16:41:25 +0700

    Upload 35 file(s) via RepoHub

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Home from './pages/index';
 import SmoothScroll from './components/scroll/SmoothScroll';
 import Loading from './components/loading/loading';
 import ScrollTopButton from './components/scrollTop/ScrollTopButton';
+import CustomCursor from './components/cursor/CustomCursor';
 
 const App = () => {
   const [loadingDone, setLoadingDone] = useState(false);
 
   return (
     <>
+      <CustomCursor />
       <Loading onFinish={() => setLoadingDone(true)} />
       <SmoothScroll>
         <Home />
